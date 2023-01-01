c7b19e26a66e15a3b36ba21dadcf69f2edde5e8f
Marcin <ssasske08@gmail.com>
2023-01-01 19:20:23 +0100

    UIpdate

---
+++ CsharpHejtoApi/Models/Background.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpHejtoApi.Models
+{
+    [Serializable]
+    public class Background
+    {
+        public BackgroundUrls? urls;
+        public string? uuid;
+        public string? alt;
+    }
+}
